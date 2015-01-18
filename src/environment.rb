00232a18192e31737a1c4834061d0e56d62443a1
MaximeDouylliez <maxime.douylliez@agil-it.fr>
2015-01-18 23:09:22 +0100

    Ajout de display, plante après le démarrage de sinatra

--- src/environment.rb
+++ src/environment.rb
@@ -6,13 +6,24 @@ class Environment < Marsys::Environment
 		super
 		calculate_expectency_actual_mean
 		end
+		display
 	end
 
 	def population_is_stable?
-		:blue.clone.concat(red:).inject(true){ |sat, el| sat && !el.has_moved? }
+		:Blue_people.clone.concat(:Red_people).inject(true){ |sat, el| sat && !el.has_moved? }
+	end
+	def display
+		puts @grid.inject(""){ |res,line| res + line.inject(""){ |res,square| res + square.char + " " } + "\n" } + "__" * @size + "\n"
+	end
+	def calculate_expectency_actual_mean
+		@expectency_actual_mean = (:Blue_people.clone.concat(:Red_people).inject{ |sum, el| sum + el.expectency_actual }.to_f)   / (:Red_people.size+ Blue_people:.size)
 	end
 
-	def calculate_expectency_actual_mean
-		@expectency_actual_mean = (:blue.clone.concat(red:).inject{ |sum, el| sum + el.expectency_actual }.to_f)   / (red:.size+ blue:.size)
-	end
+		def char
+			case @content
+				when Blue_people then "S".blue
+				when Red_people then "T".red
+				else "-"
+			end
+		end
 end
